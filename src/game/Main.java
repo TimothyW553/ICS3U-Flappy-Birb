5a480da2f9fbbbe9e0ae94c70c5ace41e31f6e8c
TimothyW553 <timothywang@crescentschool.org>
2019-05-16 08:51:37 -0400

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,9 +1,15 @@
 /* Group Members: Timothy Wang and Alireza Azimi Tabrizi
  * Game Name: Flappy Pappy
  * Work Cited Links:
- *Music:
- * https://www.sounds-resource.com/mobile/flappybird/sound/5309/
- * https://www.youtube.com/watch?v=cD8EPdn5Ctg
+ *  > Music:
+ *    > Sound Effects: https://www.sounds-resource.com/mobile/flappybird/sound/5309/
+ *    > Background Music: https://www.youtube.com/watch?v=cD8EPdn5Ctg
+ *  > Game Images:
+ *    > Play button: data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAQlBMVEX////w8PAAAAAAwmGlpaX29vacnJz7+/v78/kAvlH28vShoaEAwFmc2rKl4rvV1dWU2KwAvEmd37WUlJRlzo3/9v/joPAjAAACNklEQVR4nO3c7W6CQBRFUa3yLbXa9v1ftQp1EjsWAxycE93rvwObIldjM6sVAAAAAAAAAAAAAAAAAAAAAAAA8KL2zdZJs5cXNm9eGnnhNnXSH1sKKbzI5SuKhCueqxayLZx7Yi9UqH86z9MoCvNlJ+w8V59Apr4XHW/OWHeXTXwthR4oHEKhBwqHOA76WDOt8DJP7QZ9bOKpTrwwyYy/3ebc3CmMf2RQ6IbCGIVuKIxJC4/vvaNqwVjiwsOud1AtGEtcWG16lWrBGIUxCke5FG6qqjos87hxKTzZvatWvUJhjMJRrgq/Fhn9RoWbZUa/U+EyY4PCGIWjRIX60W9XKB8bFMYoHOV2oXT0OxZqR79lYfdMVR2CwtiLFBZriXKgsNQcoqCQwnn+KeymxTMX7r7atv2oNYfwLGzLuhYFuhaq8tYUpig8fwNWvQfP7Aqr77IshYGGhaIhEVD40ELloA+cCqWDPrAqVA76wKtQnrem8IGF6kEfuBTKB31gU6geEgGFSxeG//p61sL6o+0t8pDpJC5c1xei9WKpC5dHIYX+KKTQH4UU+qPwhm5bmlVRFFnqk78rO51l9zv+hM0xzi/7dE/MPrs/xjTdhbEvzCmkkMLUKKTwNEaLnltp9nteqzmFZ2E3M7PR3w/68R/WBgrNbtdMtt3h8xeGre3yInNShMK5ewSFre2a3Esj3wzwafcvpTAZfWF+/6APNXfrWQAAAAAAAAAAAAAAAAAAAAAAAAAA8K8f7o5QPp/pb7cAAAAASUVORK5CYII=
+ *    > Font: https://www.dafont.com/flappybirdy.font
+ *  > Code Samples:
+ *    > https://stackoverflow.com/questions/15013913/checking-collision-of-shapes-with-javafx
+ *    > https://stackoverflow.com/questions/45306039/how-to-write-lambda-expression-with-eventhandler-javafx
  */
 package game;
 
